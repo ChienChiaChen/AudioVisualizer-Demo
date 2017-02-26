9a11fd51d186e54c78e700bbd4cce14c71a972d3
ChiaChen <s124202468@gmail.com>
2017-02-26 23:31:53 +0800

    git init

---
+++ app/src/main/java/com/example/chiachen/audiovisualizer_demo/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.chiachen.audiovisualizer_demo;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.audiofx.Visualizer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	VisualizerView mVisualizerView;
+	private MediaPlayer mMediaPlayer;
+	private Visualizer mVisualizer;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		mVisualizerView = (VisualizerView)findViewById(R.id.my_visualizer);
+		initAudio();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		if (isFinishing() && mMediaPlayer != null) {
+			mVisualizer.release();
+			mMediaPlayer.release();
+			mMediaPlayer = null;
+		}
+	}
+
+	public void initAudio(){
+		setVolumeControlStream(AudioManager.STREAM_MUSIC);
+		mMediaPlayer = MediaPlayer.create(this, R.raw.test);
+
+		setupVisualizerFxAndUI();
+
+		// Make sure the visualizer is enabled only when you actually want to
+		// receive data, and
+		// when it makes sense to receive data.
+		mVisualizer.setEnabled(true);
+
+		// When the stream ends, we don't need to collect any more data. We
+		// don't do this in
+		// setupVisualizerFxAndUI because we likely want to have more,
+		// non-Visualizer related code
+		// in this callback.
+		mMediaPlayer
+				.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+					public void onCompletion(MediaPlayer mediaPlayer) {
+						mVisualizer.setEnabled(false);
+					}
+				});
+		mMediaPlayer.start();
+	}
+	private void setupVisualizerFxAndUI() {
+
+		// Create the Visualizer object and attach it to our media player.
+		mVisualizer = new Visualizer(mMediaPlayer.getAudioSessionId());
+
+		mVisualizer.setCaptureSize(Visualizer.getCaptureSizeRange()[1]);
+		mVisualizer.setDataCaptureListener(
+				new Visualizer.OnDataCaptureListener() {
+					public void onWaveFormDataCapture(Visualizer visualizer,
+					                                  byte[] bytes, int samplingRate) {
+						mVisualizerView.updateVisualizer(bytes);
+					}
+
+					public void onFftDataCapture(Visualizer visualizer,
+					                             byte[] bytes, int samplingRate) {
+					}
+				}, Visualizer.getMaxCaptureRate() / 2, true, false);
+	}
+
+}
+
